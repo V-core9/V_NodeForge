2b397c02a551a511ea7c6fa6c86a5426d5c24b23
unknown <slavko.vuletic92@gmail.com>
2021-10-16 02:36:42 +0100

    yeaa fukin filename

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ const path = require('path');
 module.exports = {
   mode: "development",
   entry: {
-    main: "./APP/SOURCE/app.js",
+    main: "./APP/SOURCE/index.js",
   },
   target: ['web'],
   output: {
     path: path.resolve(__dirname, "APP/PUBLIC"),
-    filename: "[name].v_bundle.js",
+    filename: "[name].v_bundle.js"
   }
 }
 
